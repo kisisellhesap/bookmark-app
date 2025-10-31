ea859e4ab2d77fbebf428406b0f42f3a1ee15a76
kisisellhesap <ahmetcannkaragoz@gmail.com>
2025-10-31 20:34:05 +0300

    update

--- app/firebase/index.ts
+++ app/firebase/index.ts
@@ -4,21 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-
-  apiKey: "AIzaSyDj0l77JPrDmzRI46c8i1ShVADW_XLmgP0",
-  authDomain: "bookmark-app-e64d4.firebaseapp.com",
-  projectId: "bookmark-app-e64d4",
-  storageBucket: "bookmark-app-e64d4.firebasestorage.app",
-  messagingSenderId: "781524424659",
-  appId: "1:781524424659:web:2cc47034c1607a2cdf4126",
-  measurementId: "G-VKPRGDSEBY",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
